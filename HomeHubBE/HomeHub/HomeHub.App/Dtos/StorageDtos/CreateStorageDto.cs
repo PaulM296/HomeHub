226327ef6c91a09d3d3217d529691a36da5e5a51
PaulM <paulmicluta@gmail.com>
2024-11-03 10:17:22 +0200

    Storage Dtos created

---
+++ HomeHubBE/HomeHub/HomeHub.App/Dtos/StorageDtos/CreateStorageDto.cs
@@ -0,0 +1,19 @@
+﻿using HomeHub.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeHub.App.Dtos.StorageDtos
+{
+    public class CreateStorageDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public StorageType Type { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public string? ParentStorageName { get; set; }
+    }
+}
---
+++ HomeHubBE/HomeHub/HomeHub.App/Dtos/StorageDtos/StorageResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using HomeHub.Domain.Enums;
+
+namespace HomeHub.App.Dtos.StorageDtos
+{
+    public class StorageResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public StorageType Type { get; set; }
+        public string Description { get; set; }
+        public Guid? ParentStorageId { get; set; }
+    }
+}
