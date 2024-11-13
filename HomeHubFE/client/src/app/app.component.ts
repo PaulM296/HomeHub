5f7d4e44a95c357090ad46b4115c8d8bedd41293
PaulM <paulmicluta@gmail.com>
2024-11-13 12:30:23 +0200

    Designed the not found page and restricted header's component access to the login, not found and register pages

--- HomeHubFE/client/src/app/app.component.ts
+++ HomeHubFE/client/src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'HomeHub';
+  showHeader = true;
+  headerlessRoutes: string[] = ['/not-found', '/login', '/register'];
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.showHeader = !this.headerlessRoutes.includes(this.router.url);
+    })
+  }
 }
--- HomeHubFE/client/src/app/components/not-found/not-found.component.ts
+++ HomeHubFE/client/src/app/components/not-found/not-found.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+  constructor(private titleService: Title) { }
 
+  ngOnInit(): void {
+    this.titleService.setTitle("404 Not Found");
+  }
 }
